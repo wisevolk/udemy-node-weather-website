a543cb2b4c94ec75a6483e972e22fe2a77a3aed6
Wise <wise>
2020-01-03 22:42:19 +0100

    Serving HTML and JSON

--- src/app.js
+++ src/app.js
@@ -2,19 +2,31 @@ const express = require('express');
 const app = express();
 
 app.get('', (req,res) => {
-    res.send('Hello Express!');
+    res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req,res)=>{
-   res.send('Help page')
+   res.send({
+       name: 'Wise',
+       age: '49'
+   });
 });
 
 app.get('/about', (req,res)=>{
-   res.send('About Page');
+   res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req,res)=>{
-    res.send('Informations Weather');
+    res.send({
+        forecast: {
+            temperature: '11',
+            precipProbability: '0'
+        },
+        location: {
+            city: 'Paris',
+            country: 'France'
+        }
+    });
 })
 
  app.listen(3000,()=>{
