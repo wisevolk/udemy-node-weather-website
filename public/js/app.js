bfa1ee29db57c0f1b06cf6378207a4fd72e3a2fe
Wisevolk <wisevolk@gmail.com>
2020-01-05 10:58:45 +0100

    Fetch HTTP Request

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
 console.log('Client side JS is loaded');
+
+/*
+fetch('http://puzzle.mead.io/puzzle')
+    .then(response => {
+        response.json().then(data => {
+            console.log(data);
+        })
+    })
+*/
+
+fetch('http://localhost:3001/weather?address=Boston')
+    .then(response => {
+        response.json().then(data => {
+            if (data.error)
+                console.log(data.error);
+            else
+                console.log(data);
+        });
+    });
